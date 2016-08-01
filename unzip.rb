e02f95e003d345ba23565271d7e9a806a7896846
Hirotaka Ogawa <h-ogawa@aist.go.jp>
2016-08-01 12:13:52 +0900

    make sha1 hash to sha256

--- unzip.rb
+++ unzip.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Unzip < Formula
   homepage 'http://www.info-zip.org/pub/infozip/UnZip.html'
-  url 'http://sourceforge.net/projects/infozip/files/UnZip%206.x%20%28latest%29/UnZip%206.0/unzip60.tar.gz'
+  url 'http://downloads.sourceforge.net/project/infozip/UnZip%206.x%20%28latest%29/UnZip%206.0/unzip60.tar.gz'
   version '6.0'
-  sha1 'abf7de8a4018a983590ed6f5cbd990d4740f8a22'
+  sha256 "036d96991646d0449ed0aa952e4fbe21b476ce994abc276e49d30e686708bd37"
 
   def patches
     [
